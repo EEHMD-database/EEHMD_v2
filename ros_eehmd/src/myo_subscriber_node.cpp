f24a10378f7781806492933641f503bd7d8b4cf7
SimonKirchhofer-UCA <55323805+SimonKirchhofer-UCA@users.noreply.github.com>
2020-03-10 16:35:11 +0100

    Add files via upload

---
+++ ros_eehmd/src/myo_subscriber_node.cpp
@@ -0,0 +1,24 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "EmgArray.h"
+#include "MyoArm.h"
+
+void chatterCallback(const ros_eehmd::EmgArrayConstPtr& myo_msg )
+{ for (int i=0;i<myo_msg->data.size();i++)
+  {
+  ROS_INFO("sensor number [%i] = [%i]",i+1, myo_msg->data[i]);
+  }
+}
+
+int main(int argc, char **argv)
+{
+ ros::init(argc, argv, "myo_subscriber");
+
+ ros::NodeHandle n;
+
+ros::Subscriber submyo = n.subscribe("myo_emg", 1000, chatterCallback);
+
+ ros::spin();
+
+  return 0;
+}   
